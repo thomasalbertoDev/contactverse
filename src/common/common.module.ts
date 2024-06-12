8594f345259f60ef3d41b96de773121b33102946
thomasalbertoDev <thomasalberto456@gmail.com>
2024-06-12 10:17:33 +0700

    feat: Setup coomon

---
+++ src/common/common.module.ts
@@ -0,0 +1,22 @@
+import * as winston from 'winston';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import { PrismaService } from './prisma.service';
+import { Global, Module } from '@nestjs/common';
+import { ValidationService } from './validation.service';
+
+@Global()
+@Module({
+  imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [PrismaService, ValidationService],
+  exports: [PrismaService, ValidationService],
+})
+export class CommonModule {}
